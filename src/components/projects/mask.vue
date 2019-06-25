ceeaadc3ea1426219e7db3b054e21f67f8811128
Ed1son <ed1son@Ed1sondeMacBook-Pro.local>
2019-06-25 23:02:30 +0800

    完成了专栏和项目的静态页面

---
+++ src/components/projects/mask.vue
@@ -0,0 +1,39 @@
+<template>
+    <main>
+        <div class="mask-container">
+            123
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+   
+    background-color: rgba(42, 42, 42, 0.32);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+.mask-container {
+    width: 12.8rem;
+    height: 4rem;
+    position: fixed;
+    top: 1rem;
+    left:50%;
+    margin-left: -6.4rem;
+    background-color: #fff;
+    
+}
+
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Provide } from 'vue-property-decorator'
+
+export default class Mask extends Vue {
+  
+}
+</script>
--- src/components/projects/projects.vue
+++ src/components/projects/projects.vue
@@ -1,7 +1,121 @@
 <template>
   <div>
     <main>
-        
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          智能睡眠监护枕头
+        </h1>
+        <p>
+          这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介
+        </p>
+      </div>
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “守护鲸鱼”计划游戏
+        </h1>
+        <p>
+          这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介
+          
+        </p>
+      </div>
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          面向前端的创客教育平台
+        </h1>
+        <p>
+          这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介这是简介
+          
+        </p>
+      </div>
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “单词消消乐”游戏
+        </h1>
+        <p>
+          asdlkasjdlkasjlkdjaklsdjkl
+        </p>
+      </div>
+      <div class="item item-2">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “单词消消乐”游戏
+        </h1>
+        <p>
+          asdlkasjdlkasjlkdjaklsdjkl
+        </p>
+      </div>
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “单词消消乐”游戏
+        </h1>
+        <p>
+          asdlkasjdlkasjlkdjaklsdjkl
+        </p>
+      </div>
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “单词消消乐”游戏
+        </h1>
+        <p>
+          asdlkasjdlkasjlkdjaklsdjkl
+        </p>
+      </div>
+      <div class="item item-3">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “单词消消乐”游戏
+        </h1>
+        <p>
+          asdlkasjdlkasjlkdjaklsdjkl
+        </p>
+      </div>
+      <div class="item">
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+        <h1 class="item-title">
+          “单词消消乐”游戏
+        </h1>
+        <p>
+          asdlkasjdlkasjlkdjaklsdjkl
+        </p>
+      </div>
+      <div class="item item-4">
+        <div class="content-container">
+          <h1 class="item-title">
+            矿工帽
+          </h1>
+          <p>
+            asdlkasjdlkasjlkdjaklsdjkl
+          </p>
+        </div>
+        <div class="item-img">
+          <img src="../../assets/images/2.jpg" alt="">
+        </div>
+      </div>
+      
       
     </main>
     <footer class="footer">
@@ -9,21 +123,82 @@
         <img src="../../assets/icons/up.png" alt>
       </div>
     </footer>
+    <!-- <project-mask></project-mask> -->
   </div>
 </template>
 
 
-<script >
-export default {
-  name: "projects",
-  data: {}
-};
-</script >
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import Mask from './mask.vue'
+
+@Component({
+  components: {
+    'project-mask': Mask
+  }
+})
+export default class Projects extends Vue {
+  mouted() {
+    // document.getElementsByClassName('el-scrollbar__wrap')[0].style.overflowX = 'hidden';
+  }
+}
+</script>
 
 
 
-<style lang='css' >
+<style lang='css' scoped>
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.item {
+  height: 6.4rem;
+  width: 3.6rem;
+  box-shadow: 0 .06rem .12rem 0 #ded5d5;
+  text-align: left;
+  margin: .24rem .24rem
 
+}
+.item-img img {
+  height: 2rem;
+  width: 100%;
+}
+.item-title {
+  padding: .26rem;
+  font-size: .24rem;
+  padding-bottom: .1rem;
+  font-weight: 500;
+  color: #2a2a2a;
+}
+.item p {
+  padding: 0 .26rem ;
+  padding-top: 0;
+  font-size: .2rem;
+  color: #707070;
+  overflow: hidden;
+  height: 3.4rem;
+}
+
+.item-2 {
+  width: 7.68rem;
+
+}
+.item-3 {
+  width: 11.76rem;
+}
+.item-4 {
+  width: 15.84rem;
+  display: flex; 
+}
+.item-4 .content-container {
+  flex: 1;
+}
+.item-4 .item-img  img{
+  width: 7.64rem;
+  height: 6.4rem;
+}
 
 .backtotop-button {
   margin: 2rem auto .5rem auto;
