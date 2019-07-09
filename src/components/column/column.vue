9bc2378e7efe9c54ba442a76bf538f77a75d61d5
Ed1son <ed1son@Ed1sondeMacBook-Pro.local>
2019-07-09 11:00:58 +0800

    修改了大事件的内容

--- src/components/column/column.vue
+++ src/components/column/column.vue
@@ -55,71 +55,121 @@
       <h1 class="second-header" id="importantEvent">QG大事件</h1>
       <section class="part-2">
         <div class="part-2-left">
-          <div class="card-2">
-            <div class="card-2-img-container">
-              <img src="../../assets/images/column/importantevent/2005.9.jpg" alt>
-            </div>
-            <div class="time-container">
-              2005年9月
-            </div>
-            <div class="part-2-content-container">
-              <p>QG工作室成立</p>
-            </div>
-          </div>
-          <div class="card-2">
-            <div class="card-2-img-container">
-              <img src="../../assets/images/column/importantevent/2015.7.jpg" alt>
-            </div>
-            <div class="time-container">
-              2015年7月
-            </div>
-            <div class="part-2-content-container">
-              <p>为广工大首次获得了第十四届“挑战杯”国赛“特等奖”</p>
-            </div>
-          </div>
-          <!-- <div class="card-2">
-            <div class="card-2-img-container">
-              <img src="../../assets/images/2.jpg" alt>
-            </div>
-            <div class="time-container">
-              2005.X.X
-            </div>
-            <div class="part-2-content-container">
-              <p>这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容这个是内容</p>
-            </div>
-          </div> -->
-        </div>
-        <div class="timeline-container">
-          <div class="start"></div>
-          <div class="line"></div>
-          <div class="arrow">
-        <img src="../../assets/icons/up.png" alt>
 
+          <!-- 工作室成立 -->
+          <div class="card-2">
+            <div class="card-2-img-container">
+              <img src="../../assets/images/column/importantevent/工作室成立.png" alt>
+            </div>
+            <div class="title-container">
+              <div class="title">
+                工作室成立
+              </div>
+            </div> 
+            <div class="part-2-content-container">
+              <p>致力于培养更多品学兼优、专业知识扎实、能独挡一面的创新型人才，于2005年创立了大学生科技创新团队“QG工作室”，至今已有14个年头。初心：对于这个团队，我一直坚持一个梦想，就是要把团队打造成为国内顶尖的大学生科技创新团队。能够代表广东工业大学，代表广东省，站在全国大学生科技竞赛的舞台上展示工大学子的实力和风采！</p>
+            </div>
           </div>
-        </div >
-        <div class="part-2-right">
+          <!-- 工作室成立 -->
+          <!-- 挑战杯 -->
           <div class="card-2">
             <div class="card-2-img-container">
               <img src="../../assets/images/column/importantevent/2011.10.jpg" alt>
             </div>
-            <div class="time-container">
-              2011年10月
+            <div class="title-container">
+              <div class="title">
+                首次突破
+              </div>
+              
             </div>
             <div class="part-2-content-container">
-              <p>为广工大首次获得了第十二届“挑战杯”国赛“一等奖”</p>
+              <p>渐渐地，略有收获，不断进步，小奖项  、 大奖项……大满贯！2011年10月，梦想终于实现，工作室研发成果经过层层筛选，代表广东省参加第十二届“挑战杯”全国大学生竞赛终审决赛。不负众望，为广工大首次获得了“挑战杯”国赛“一等奖” 。作品是三年磨一剑，由三届团队成员合力完成。一切的努力都是值得的！</p>
             </div>
           </div>
           <div class="card-2">
             <div class="card-2-img-container">
               <img src="../../assets/images/column/importantevent/2018.8.jpg" alt>
             </div>
-            <div class="time-container">
-              2018年8月
+            <div class="title-container">
+              <div class="title">
+                第三次重要突破
+              </div>
             </div>
             <div class="part-2-content-container">
-              <p>颁获“大学生小平科技创新团队”荣誉称号</p>
+              <p>邓小平同志诞辰114周年之际，第十一届中国青少年科技创新奖颁奖大会于8月24日在人民大会堂举行。“ QG工作室”获颁“大学生小平科技创新团队”荣誉称号。中共中央政治局委员、国务院副总理孙春兰出席并讲话，为获奖代表颁奖。该称号以邓小平同志命名，由共青团中央、全国青联、全国学联、全国少工委、中国青少年科技创新奖励基金共同设立, 旨在面向全国大学生遴选在学术研究、科技竞赛、成果转化等方面取得突出成绩或显示较大潜力的大学生科技创新团队给予支持。全国共有50个大学生创新团队获此项殊荣。（省内仅广工与华工两个团队）。</p>
             </div>
           </div>
+          <div class="card-2">
+            <div class="card-2-img-container">
+              <img src="../../assets/images/column/importantevent/新闻报道.png" alt>
+            </div>
+            <div class="title-container">
+              <div class="title">
+                新闻报道工作室
+              </div>
+            </div>
+            <div class="part-2-content-container">
+              <p>后来，不仅仅只有知名人士来访问工作室，还有电视台采访我们工作室，作品受邀参加全国大学生创新创业年会。工作室所获成果受到了广东卫视、广州日报等主流媒体的关注和报道。接受了广东卫视专访,介绍了研发成果，采访内容在《广东新闻联播》中播出。作品还获得了广东省教育厅推荐，代表广东省参加第十届全国大学生创新创业年会。</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="timeline-container">
+          <div class="start"></div>
+          <div class="line"></div>
+          <div class="arrow">
+            <img src="../../assets/icons/up.png" alt>
+          </div>
+        </div >
+
+        <div class="part-2-right">
+
+          <div class="card-2">
+            <div class="card-2-img-container">
+              <img src="../../assets/images/column/importantevent/首次出征.png" alt>
+            </div>
+            <div class="title-container">
+              <div class="title">
+                首次出师，失败的尝试
+              </div>
+              
+            </div>
+            <div class="part-2-content-container">
+              <p>团队第一次参赛：作品在比赛前出现了故障，无法工作。结果可想而知，3个月团队的努力以失败告终。出师不利，铩羽而归，士气很低落，一度想放弃。我告诉学生，失败不可怕，可怕的是认为自己不行。从哪跌倒就从哪爬起来。重新振作，总结原因。制定一系列有效的措施和方法。</p>
+            </div>
+          </div>
+
+          <div class="card-2">
+            <div class="card-2-img-container">
+              <img src="../../assets/images/column/importantevent/2015.7.jpg" alt>
+            </div>
+            <div class="title-container">
+              <div class="title">
+                第二次重要突破
+              </div>
+              
+            </div>
+            <div class="part-2-content-container">
+              <p>2015年7月，第十四届“挑战杯”全国大学生课外学术科技作品竞赛，"智慧城市"专项赛决赛，工作室研发的科技作品，从全国一千多件科技作品脱颖而出，决赛在香港科技大学举行。团队十几位成员，齐心协力，多方向协同创新云系统开发、UI设计、移动APP、嵌入式系统白天开发，晚上讨论，没日没夜的工作。决赛所有的材料、现场答辩都全英文要求。答辩PPT改版了上百次。英文讲稿改稿几十版。现场改稿，脱稿演讲精益求精的工匠精神，追求做最好的自己！</p>
+            </div>
+          </div>
+
+          <div class="card-2">
+            <div class="card-2-img-container">
+              <img src="../../assets/images/column/importantevent/访问工作室.png" alt>
+            </div>
+            <div class="title-container">
+              <div class="title">
+                知名人士访问工作室
+              </div>
+              
+            </div>
+            <div class="part-2-content-container">
+              <p>渐渐地，我们工作室有了外界的关注。广东省政协主席王荣、教育部原副部长鲁昕、学校校长陈新听取工作室介绍。</p>
+            </div>
+          </div>
+
+          
         </div>
       </section>
     </main>
@@ -236,7 +286,8 @@
   
 }
 .line {
-  height: 17rem;
+  /* 在这里修改时间轴的高度 */
+  height: 31rem;
   background-color: #707070;
   width: .04rem;
   margin: 0 auto;
@@ -253,15 +304,19 @@
 .card-2 {
   box-shadow: 0 .06rem .12rem 0 #ded5d5;
   width: 7.2rem;
-  height: 4.8rem;
+  /* min-height: 4.8rem; */
   overflow: hidden;
 }
 .card-2-img-container img{
   width: 7.2rem;
-  height: 2.8rem;
+  height: 3.6rem;
 }
-.time-container {
-  width: 2rem;
+.title-container {
+  width: 100%;
+}
+.title {
+  width:fit-content;
+  padding: 0 .2rem;
   height: .48rem;
   background-color: #5a0bc3;
   margin: .16rem 0;
@@ -269,15 +324,16 @@
   color: #fff;
   font-size: .24rem;
   line-height: .48rem;
+  overflow: auto;
 }
 .part-2-content-container {
-  padding-top: 0;
-  height: 1rem;
+  /* height: 1rem; */
+  height: auto;
   overflow: hidden;
-  padding: 0 .3rem;
+  padding: 0 .3rem 0.2rem 0.3rem;
 }
 .part-2-content-container p {
-  font-size: .32rem;
+  font-size: .2rem;
   color: #2a2a2a;
   text-align: left;
   width: 100%;  
